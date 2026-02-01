4cd51cbc41b640426ed2014c5fe88ee490893ff1
Yoni-Sabag <yonisabag11@gmail.com>
2026-02-01 15:50:18 +0200

    Remove base path - simplify for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -8,26 +8,21 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === 'build' ? '/portfolio/' : '/'
-  
-  return {
-    base,
-    plugins: [
-      vue(),
-      VueDevTools(),
-      vueJsx(),
-      VueI18nPlugin({
-        include: resolve(dirname(fileURLToPath(import.meta.url)), 'src/locales/**'),
-      })
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    build: {
-      sourcemap: true,
-    },
-  }
+export default defineConfig({
+  plugins: [
+    vue(),
+    VueDevTools(),
+    vueJsx(),
+    VueI18nPlugin({
+      include: resolve(dirname(fileURLToPath(import.meta.url)), 'src/locales/**'),
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    sourcemap: true,
+  },
 })
