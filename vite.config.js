8848d55641bc18fa711073ee54917bd606bdd06a
Yoni-Sabag <yonisabag11@gmail.com>
2026-02-01 17:18:49 +0200

    Restore base path /portfolio/ - required for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [
     vue(),
     VueDevTools(),
